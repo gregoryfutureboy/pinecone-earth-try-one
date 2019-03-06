86fe429ed260e6ca926c5ad07a179fc3d7df7646
Futureboy <gregory@human.space>
2019-03-06 14:09:40 -0500

    graphcms integrated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloLink } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
-import { onError } from 'apollo-link-error'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo';
 
 import App from './App';
 
--- src/pages/LandingPage/Landing.jsx
+++ src/pages/LandingPage/Landing.jsx
@@ -1,11 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const GET_LANDING = gql`
+  query {
+    landingPages {
+      h1
+      h3
+    }
+  }
+`
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        Welcome to the future
-      </div>
+      <Query query={GET_LANDING}>
+        {
+          ({ data, loading, error }) => {
+            console.log(data)
+            if(error) {
+              return <div>error</div>
+            }
+
+            const { landingPages } = data;
+            if(loading || !landingPages) {
+              return <div>Loading</div>
+            }
+
+            return (
+              <h1>
+                {landingPages[0].h1}
+              </h1>
+            )
+          }
+        }
+      </Query>
     )
   }
 }
